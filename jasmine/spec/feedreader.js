80d233463602e91ce2a2b188ba1495c9616785f5
shawnajean <shawnajean@users.noreply.github.com>
2016-02-07 14:21:50 -0600

    add initial entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,22 +67,42 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('displays when menu icon is clicked', function() {
 
+          // Click on icon -> Displays menu
+          $('.menu-icon-link').click();
+          expect( $('body').hasClass('menu-hidden') ).toBe(false);
+
+          //Click on icon 2nd time -> Hides menu
+          $('.menu-icon-link').click();
+          expect( $('body').hasClass('menu-hidden') ).toBe(true);
+        });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+      beforeEach( function( done ) {
+        loadFeed(0, done);
+      });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+      it('have at least one entry on load', function() {
+        expect( $('.entry').length ).toBeGreaterThan(0);
+      });
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+      /* TODO: Write a test that ensures when a new feed is loaded
+       * by the loadFeed function that the content actually changes.
+       * Remember, loadFeed() is asynchronous.
+       */
+       
+    });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
 }());
