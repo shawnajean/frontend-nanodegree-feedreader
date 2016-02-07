3a117a03393eed058b5366bd3e61804dfb677dd1
shawnajean <shawnajean@users.noreply.github.com>
2016-02-07 13:41:14 -0600

    add feed-specific tests and menu display test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,44 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('have URLs', function() {
+          allFeeds.forEach( function(feed) {
+            expect(feed.url.length).toBeGreaterThan(0);
+          });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have names', function() {
+          allFeeds.forEach( function(feed) {
+            expect(feed.name.length).toBeGreaterThan(0);
+          });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        // Menu is hidden when the body element has the class 'menu-hidden'
+        it('is hidden on load', function() {
+          expect( $('body').hasClass('menu-hidden') ).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
